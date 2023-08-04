921d085282fb92466bc10213f359291b686bb60b
Devin Dyer <dyerda112@gmail.com>
2023-08-03 20:19:43 -0400

    add a note

--- JSON/JSON.test.cpp
+++ JSON/JSON.test.cpp
@@ -19,13 +19,5 @@ TEST_CASE("object", "[object]")
     JSONValue val = obj2->get("baz");
     val.print();
 
-    /* Need to take a step back. Had to delete a bunch of work that I did
-            because 99% of it was done without designing or thoughtful consideration.
-            I should start testing and working on arrays the next time I work on this. Just worry
-            about getting it working with pointer and new.
-
-            Once I start thinking about how I will use these classes in the future I will
-            start worrying about what constructors methods and variations of those I need to implement.
-            But to start I need to get it working the way I know it will.
-    */
+    // Need to make sure I'm handling the memory correctly and consistently.
 }

c5fdd2a9e04e9fc54578e71f70137701b60e7f5e
Devin Dyer <dyerda112@gmail.com>
2023-08-07 21:33:15 -0400

    update json tests

--- JSON/JSON.test.cpp
+++ JSON/JSON.test.cpp
@@ -7,6 +7,7 @@
 // ===================
 // Write more tests! Need to make sure that everything is
 // complete before continuing with parser.
+// How can I cover all possibilities?
 // ===================
 
 TEST_CASE("Object", "[Object]")
@@ -41,25 +42,7 @@ TEST_CASE("Nested value equals", "[Nested value equals]")
     REQUIRE(val.equals(val2));
 }
 
-TEST_CASE("Nested value equals different value", "[Nested value equals different value]")
-{
-    Array<JSONValue> *arr = new Array<JSONValue>();
-    JSONValue *val1 = new JSONValue("foo");
-    arr->push(val1);
-    arr->push(val1);
-    arr->push(val1);
-    JSONValue *val2 = new JSONValue(arr);
-    JSONObject *obj1 = new JSONObject("bar", val2);
-    JSONValue *val3 = new JSONValue(obj1);
-    JSONObject *obj2 = new JSONObject("baz", val3);
-
-    JSONValue *val4 = new JSONValue(arr);
-
-    JSONValue val = obj2->get("bar");
-    REQUIRE(val.equals(val4));
-}
-
-TEST_CASE("Nested value not equal to different value", "[Nested value not equal to different value]")
+TEST_CASE("Nested array equals different value", "[Nested array equals different value]")
 {
     Array<JSONValue> *arr1 = new Array<JSONValue>();
     JSONValue *val1 = new JSONValue("foo");
@@ -72,11 +55,35 @@ TEST_CASE("Nested value not equal to different value", "[Nested value not equal 
     JSONObject *obj2 = new JSONObject("baz", val3);
 
     Array<JSONValue> *arr2 = new Array<JSONValue>();
-    arr2->push(JSONValue("foo"));
-    arr2->push(JSONValue("foo"));
-    arr2->push(JSONValue("foo"));
+    JSONValue *val4 = new JSONValue("foo");
+    arr2->push(val4);
+    arr2->push(val4);
+    arr2->push(val4);
     JSONValue val5 = new JSONValue(arr2);
 
     JSONValue val = obj2->get("bar");
+    REQUIRE(val.equals(val5));
+}
+
+TEST_CASE("Nested array value equals value", "[Nested array value equals value]")
+{
+    Array<JSONValue> *arr1 = new Array<JSONValue>();
+    JSONValue *val1 = new JSONValue("foo");
+    arr1->push(val1);
+    arr1->push(val1);
+    arr1->push(val1);
+    JSONValue *val2 = new JSONValue(arr1);
+    JSONObject *obj1 = new JSONObject("bar", val2);
+    JSONValue *val3 = new JSONValue(obj1);
+    JSONObject *obj2 = new JSONObject("baz", val3);
+
+    Array<JSONValue> *arr2 = new Array<JSONValue>();
+    JSONValue *val4 = new JSONValue("foo");
+    arr2->push(val4);
+    arr2->push(val4);
+    arr2->push(val4);
+    JSONValue val5 = new JSONValue(arr2);
+
+    JSONValue val = val3->get("bar");
     REQUIRE(val.equals(val5));
 }

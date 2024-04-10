2cd0053e18dc87ceba8ffafe7808cdbdd5a8afde
Devin Dyer <73308649+devinaustindyer112@users.noreply.github.com>
2024-04-09 21:27:15 -0400

    fix-casing (#1)
    * testing ls-tree
    
    * testing ls-tree
    
    * testing ls-tree
    
    * updates to git-to-lowercase script
    
    * update to lowercase script
    
    * fix ls-files command
    
    * add comment
    
    * update git-to-lowercase script
    
    * JFA to jfa
    
    * jfa/Array/Array.cpp to jfa/array/array.cpp
    
    * jfa/Array/Array.hpp to jfa/array/array.hpp
    
    * jfa/Array/Array.test.cpp to jfa/array/array.test.cpp
    
    * jfa/Map/Map.cpp to jfa/map/map.cpp
    
    * jfa/Map/Map.hpp to jfa/map/map.hpp
    
    * jfa/Map/Map.test.cpp to jfa/map/map.test.cpp
    
    * jfa/Parser/Parser.cpp to jfa/parser/parser.cpp
    
    * jfa/Parser/Parser.hpp to jfa/parser/parser.hpp
    
    * jfa/Parser/Parser.test.cpp to jfa/parser/parser.test.cpp
    
    * jfa/Tokenizer/Tokenizer.cpp to jfa/tokenizer/tokenizer.cpp
    
    * jfa/Tokenizer/Tokenizer.hpp to jfa/tokenizer/tokenizer.hpp
    
    * jfa/Tokenizer/Tokenizer.test.cpp to jfa/tokenizer/tokenizer.test.cpp
    
    * add git mv logic

--- git-to-lowercase.sh
+++ git-to-lowercase.sh
@@ -1,22 +1,38 @@
 #!/bin/bash
 
-# TODO: Delete this file
-# Only needed for forcing directories and files to 
-# lowercase
+# TODO: Move this file
+# Only needed for forcing directories and files to lowercase
+
+# TODO: Research why OS being case insensitive causes discrepencies
+# https://stackoverflow.com/questions/3011625/git-mv-and-only-change-case-of-directory
+
+# QUESTION: How do people normally style bash?
 
 to_lowercase() {
-    basename $1 
+    lowercase=$(echo "$1" | tr '[:upper:]' '[:lower:]')
+    echo "$1 to $lowercase"
+    
+    git mv $1 "$1-tmp"
+    git mv "$1-tmp" $lowercase
+    git commit -m "$1 to $lowercase"
 }
 
 export -f to_lowercase
 
-find . \
-  -not -path "./git-to-lowercase*" \
-  -not -path "." \
-  -not -path "./.git*" \
-  -not -path "./.vscode*" \
-  -not -path "./Makefile" \
-  -not -path "./README.md" \
-  -not -path "./catch_*" \
-  -exec bash -c 'to_lowercase "$0"' {} \;
+echo "------- rename directories -------"
 
+# Rename directories
+directories=($(git ls-tree -r -d --name-only HEAD | uniq ))
+for directory in "${directories[@]}"
+do
+    echo "$(to_lowercase $directory)"
+done
+
+# echo "------- rename files -------"
+
+# # Rename files
+# files=($(git ls-files | uniq | grep -v 'Makefile\|README.md'))
+# for file in "${files[@]}"
+# do
+#     echo "$(to_lowercase $file)"
+# done
